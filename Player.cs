aa5fc351973ce73ecca440f8f2761d2cfbc744bd
Unity Project <bebless@kakao.com>
2024-04-20 23:40:30 +0900

    Update

--- Player.cs
+++ Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -8,6 +10,8 @@ public class Player : MonoBehaviour
 {
   public uint level ;
 
+
+ 
   [SerializeField]  private uint moveSpeed;
   void Start()
   {
@@ -15,8 +19,39 @@ public class Player : MonoBehaviour
     moveSpeed = 3;
     
   }
-  private void Update() {
-    if(Input.GetMouseButton(0))
+  private void Update() 
+  {
+
+
+    MaouseMove_1();
+
+  }
+
+
+    void MaouseMove_1()
+    {
+      
+      if(Input.GetMouseButton(0))
+      {
+        Vector3 clickPos;
+        //method
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition + new Vector3(0,1f,0));
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit))
+        {
+          clickPos = hit.point;          
+          Debug.Log(clickPos);
+          gameObject.transform.position = clickPos ;
+        }
+        
+        
+        
+      }
+    }
+    void MouseMove_2()
+    {
+      if(Input.GetMouseButton(0))
       {
         Vector3 clickPos = Vector3.one;
         clickPos  = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,1f,0 ));
@@ -26,8 +61,5 @@ public class Player : MonoBehaviour
         
         gameObject.transform.position = clickPos * moveSpeed * Time.deltaTime;
       }
-
-    
-
     }
 }
